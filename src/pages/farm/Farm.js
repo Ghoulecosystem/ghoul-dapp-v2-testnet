9280303109f628b9b87815ef448416c6650ac542
James Snewin <jamessnewin0@gmail.com>
2022-01-25 11:06:33 +1000

    farm web3 logic finished

--- src/pages/farm/Farm.js
+++ src/pages/farm/Farm.js
@@ -6,11 +6,16 @@ import StakeLpModal from "../../components/stake-lp-modal/StakeLpModal";
 import Accordion from "../../components/farm-accordion/FarmAccordion";
 import Web3Context from "../../store/Web3-context";
 import { ethers } from "ethers";
-import { lpAbi, farmAddress } from "../../utils/contract_abis_mainnet";
+import {
+  lpAbi,
+  farmAddress,
+  ghoulxAddress,
+} from "../../utils/contract_abis_mainnet";
 import { parseEther } from "ethers/lib/utils";
 import LoadingImg from "../../components/loading-img-component/LoadingImg";
 import SnackbarUI from "../../components/snackbar/SnackbarUI";
 import ThemeContext from "../../store/Theme-context";
+const axios = require("axios");
 
 const modalStyle = {
   overlay: {
@@ -94,7 +99,7 @@ const Farm = () => {
 
     let pools = [];
     try {
-      for (let i = 1; i < poolCountFormat; i++) {
+      for (let i = 0; i < poolCountFormat; i++) {
         let pool = {};
         pool.id = i;
         const poolInfo = await web3Ctx.farmContract.poolInfo(i);
@@ -139,6 +144,7 @@ const Farm = () => {
           lpAbi,
           web3Ctx.signer
         );
+
         pool.lpName = await lpContract.name();
         pool.lpSymbol = await lpContract.symbol();
         pool.lpBalance = await lpContract.balanceOf(web3Ctx.walletAddress);
@@ -150,15 +156,158 @@ const Farm = () => {
         pool.lpAllowance = ethers.utils.formatEther(pool.lpAllowance);
         pool.lpApproved = parseInt(pool.lpAllowance) > 0;
         pool.lpBalanceContract = await lpContract.balanceOf(farmAddress);
-        console.log(pool.lpBalanceContract);
         pool.lpBalanceContract = parseFloat(
           ethers.utils.formatEther(pool.lpBalanceContract)
         ).toFixed(4);
+        pool.weight = (
+          (parseFloat(pool.despositedAmount) /
+            parseFloat(pool.lpBalanceContract)) *
+          100
+        ).toFixed(4);
+        pool.isLp = false;
+        const totalSupply = await lpContract.totalSupply();
+        const totalSupplyFormat = ethers.utils.formatEther(totalSupply);
+        let farmSupply = 0;
+        if (pool.lpSymbol == "Cake-LP") {
+          pool.isLp = true;
+          pool.token0 = await lpContract.token0();
+          pool.token1 = await lpContract.token1();
 
-        pool.weight = parseFloat(pool.despositedAmount);
+          const token1 = new ethers.Contract(
+            pool.token0,
+            lpAbi,
+            web3Ctx.signer
+          );
+          const token2 = new ethers.Contract(
+            pool.token1,
+            lpAbi,
+            web3Ctx.signer
+          );
 
-        pools.push(pool);
+          let sym1 = await token1.symbol();
+          let sym2 = await token2.symbol();
+          let name1 = await token1.name();
+          let name2 = await token2.name();
+          farmSupply = await lpContract.balanceOf(farmAddress);
+          farmSupply = ethers.utils.formatEther(farmSupply);
+          pool.totalSupply = totalSupplyFormat;
+          pool.farmSupply = farmSupply;
+          if (sym1 == "WBNB") {
+            sym1 = "BNB";
+          }
+          if (sym2 == "WBNB") {
+            sym2 = "BNB";
+          }
+          if (name1 == "Wrapped BNB") {
+            name1 = "BNB";
+          }
+          if (name2 == "Wrapped BNB") {
+            name2 = "BNB";
+          }
+          if (sym2 == "BNB") {
+            sym2 = sym1;
+            name2 = name1;
+            sym1 = "BNB";
+            name1 = "BNB";
+          }
+
+          pool.lpSymbol = sym1 + "/" + sym2;
+          pool.lpName = name1 + "/" + name2;
+          let token0price = 0;
+          let token1price = 0;
+
+          try {
+            token0price = await axios.get(
+              "https://api.pancakeswap.info/api/v2/tokens/" + pool.token0
+            );
+            token1price = await axios.get(
+              "https://api.pancakeswap.info/api/v2/tokens/" + pool.token1
+            );
+            pool.token0price = token0price.data.data.price;
+            pool.token1price = token1price.data.data.price;
+          } catch (error) {
+            console.log(error);
+            pool.token0price = 0;
+            pool.token1price = 0;
+          }
+
+          let balance0 = await token2.balanceOf(pool.lpToken);
+          balance0 = ethers.utils.formatEther(balance0);
+          let balance1 = await token1.balanceOf(pool.lpToken);
+          balance1 = ethers.utils.formatEther(balance1);
+
+          console.log(balance0);
+          console.log(balance1);
+          console.log(totalSupplyFormat);
+          pool.lpPrice =
+            (parseFloat(pool.token0price) * parseFloat(balance1) +
+              parseFloat(pool.token1price) * parseFloat(balance0)) /
+            parseFloat(totalSupplyFormat);
+
+          pool.poolValue = pool.lpPrice * parseFloat(farmSupply);
+        } else {
+          farmSupply = await lpContract.balanceOf(farmAddress);
+          farmSupply = ethers.utils.formatEther(farmSupply);
+          pool.farmSupply = farmSupply;
+
+          const result = await axios
+            .get("https://api.pancakeswap.info/api/v2/tokens/" + pool.lpToken)
+            .then((res) => {
+              pool.lpPrice = res.data.data.price;
+              pool.poolValue = pool.lpPrice * parseFloat(farmSupply);
+            });
+        }
+
+        pool.allowance = await lpContract.allowance(
+          web3Ctx.walletAddress,
+          farmAddress
+        );
+        pool.allowance = ethers.utils.formatEther(pool.allowance);
+        if (parseInt(pool.allowance > 0)) {
+          pool.approved = true;
+        } else {
+          pool.approved = false;
+        }
+
+        let result = {};
+        let ghouxPrice = 0;
+
+        try {
+          result = await axios.get(
+            "https://api.pancakeswap.info/api/v2/tokens/" + ghoulxAddress
+          );
+          if (result.data) {
+            ghouxPrice = result.data.data.price;
+          }
+        } catch (error) {
+          console.log(error);
+        }
+
+        pool.apy =
+          ((31536000 * parseInt(pool.pullAllocationPercentage)) /
+            100 /
+            parseFloat(pool.poolValue)) *
+          parseFloat(ghouxPrice) *
+          100;
+
+        if (
+          pool.token0 === "0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c" ||
+          pool.token1 === "0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c"
+        ) {
+          if (pool.token0 === "0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c") {
+            pool.link0 = "BNB";
+            pool.link1 = pool.token1;
+          } else {
+            pool.link0 = "BNB";
+            pool.link1 = pool.token0;
+          }
+        }
+        if (parseInt(pool.pullAllocationPercentage) > 0) {
+          pools.push(pool);
+        }
       }
+
+      console.log(pools);
       setPools(pools);
       setIsLoadingPools(false);
     } catch (error) {
